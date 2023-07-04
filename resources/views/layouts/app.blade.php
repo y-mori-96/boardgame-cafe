5550de5114bd6ba77f24b04628ee1ab9ea123b10
Yuta mori <y.mori2101@gmail.com>
2023-07-04 21:56:28 +0900

    ファビコン設定

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="robots" content="noindex , nofollow" />
+        
 
         <title>{{ config('app.name', 'ボードゲームカフェ') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="icon" href="{{asset('img/favicon.png')}}" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
